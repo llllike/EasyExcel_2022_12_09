2cdcb3f5886f2fd67a5de19268010f510815c2bd
llllike <100058012+llllike@users.noreply.github.com>
2022-12-09 16:46:13 +0800

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,20 @@
+create table mybatisplus_db.user
+(
+    id       int auto_increment,
+    name     varchar(255)     not null,
+    password varchar(255)     not null,
+    age      int(5)           not null,
+    tel      varchar(40)      not null,
+    deleted  int(1) default 0 not null,
+    version  int(20)          not null,
+    sex      int(1)           not null,
+    constraint user_id_uindex
+        unique (id),
+    constraint user_tel_uindex
+        unique (tel)
+);
+
+alter table mybatisplus_db.user
+    add primary key (id);
+
+
